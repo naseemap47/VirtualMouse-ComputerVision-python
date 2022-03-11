46073ca3cf0ae9e6758bd7484e391baf8ce276cd
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-11 22:11:43 +0530

    draw rectangle box around hand

--- virtualmouse.py
+++ virtualmouse.py
@@ -20,13 +20,25 @@ while True:
     result = hand.process(img_rgb)
     if result.multi_hand_landmarks:
         lm_list = []
+        x_list = []
+        y_list = []
         for hand_lm in result.multi_hand_landmarks:
             for id, lm in enumerate(hand_lm.landmark):
                 height, width, channel = img.shape
                 x, y = int(lm.x * width), int(lm.y * height)
+                x_list.append(x)
+                y_list.append(y)
                 lm_list.append([id, x, y])
-                print(lm_list)
+                # print(lm_list)
                 mp_draw.draw_landmarks(img, hand_lm, mp_hand.HAND_CONNECTIONS)
+                x_min, x_max = min(x_list), max(x_list)
+                y_min, y_max = min(y_list), max(y_list)
+            cv2.rectangle(
+                img, (x_min - 20, y_min - 20),
+                (x_max + 20, y_max + 20),
+                (0, 255, 0), 2
+            )
+
     c_time = time.time()
     fps = 1 / (c_time - p_time)
     p_time = c_time
