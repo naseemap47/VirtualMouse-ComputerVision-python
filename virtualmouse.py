5bb7a2a28669ba287218b0d4391b30c9e2663de2
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-11 21:57:19 +0530

    displayed frame per sec

--- virtualmouse.py
+++ virtualmouse.py
@@ -7,9 +7,18 @@ cap = cv2.VideoCapture(0)
 cap.set(3, height_cam)
 cap.set(4, width_cam)
 
+p_time = 0
 
 while True:
     success, img = cap.read()
 
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+    cv2.putText(
+        img, f'FPS: {int(fps)}', (10, 50),
+        cv2.FONT_HERSHEY_PLAIN, 2,
+        (255, 0, 0), 2
+    )
     cv2.imshow("Image", img)
     cv2.waitKey(1)
